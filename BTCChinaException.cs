495408db2c69838c8613638f4e2ea94de3ccc0a5
happyili <happyili@hotmail.com>
2014-07-04 14:04:33 +0800

    Add c# API class and sample code. remove the old one.

---
+++ BTCChinaException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace BTCChina
+{
+    [Serializable()]
+    public class BTCChinaException : Exception
+    {
+        private string method;
+        private string jsonID;
+
+        public BTCChinaException() : base() { }
+        public BTCChinaException(string callerMethod, string callerID, string message, Exception innerException):base(message,innerException)
+        {
+            method = callerMethod;
+            jsonID = callerID;
+        }
+        public BTCChinaException(string callerMethod, string callerID, string message):base(message)
+        {
+            method = callerMethod;
+            jsonID = callerID;
+        }
+        public BTCChinaException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        public string RequestMethod { get { return method; } }
+        public string RequestID { get { return jsonID; } }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace BTCChina
+{
+    class Program
+    {
+        /// <summary>
+        /// Takes a file as argument, whose
+        /// first line is access key (public key) of your account, and
+        /// second line is secret key of your account.
+        /// </summary>
+        /// <param name="args">name of the file</param>
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: BTCChina.exe account_filename");
+            }
+            else
+                if (File.Exists(args[0]))
+                {
+                    using (StreamReader sr = File.OpenText(args[0]))
+                    {
+                        string accesskey = sr.ReadLine();
+                        string secretkey = sr.ReadLine();
+                        BTCChinaAPI testAPI = new BTCChinaAPI(accesskey, secretkey);
+                        try
+                        {
+                            string result1 = testAPI.getAccountInfo();
+                            string result2 = testAPI.getOrders(false);
+                            Console.WriteLine(result2);
+                            Console.WriteLine(result1);
+                        }
+                        catch (BTCChinaException ex)
+                        {
+                            Console.WriteLine("\nin method:" + ex.RequestMethod + "\nid:" + ex.RequestID + "\nmessage:" + ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error reading acc.txt");
+                }
+        }
+    }
+}
